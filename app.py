252c4d6e9b12192fc2b9c6a936bba0a4ac78f6e1
sveder <m@sveder.com>
2025-11-07 22:08:07 +0000

    Update Riot API key

--- app.py
+++ app.py
@@ -97,7 +97,7 @@ def cached_get(url, headers_tuple):
 
 # You need to get your API key from https://developer.riotgames.com/
 RIOT_API_KEY = os.environ.get('RIOT_API_KEY', 'YOUR_API_KEY_HERE')
-RIOT_API_KEY = "RGAPI-919b971a-d901-4382-a771-9cad5515b5dd"
+RIOT_API_KEY = "RGAPI-6d3ff612-bef2-47ed-994d-85666927612c"
 
 # Regional routing values
 REGION_ROUTING = {
